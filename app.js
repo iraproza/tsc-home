8be345e89231ac51eaf3845fa748ae7d47d5655c
Iraproza <kolomiira@gmail.com>
2021-05-22 17:40:02 +0300

    print

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var Flat = /** @class */ (function () {
-    function Flat(floors, Floor, rooms, area, date, district, description, price) {
+    function Flat(name, floors, Floor, rooms, area, date, district, description, price) {
+        this.name = name;
         this.floors = floors;
         this.Floor = Floor;
         this.rooms = rooms;
@@ -13,32 +14,50 @@ var Flat = /** @class */ (function () {
         var block = document.createElement('h1');
         var tab = document.querySelector('.table');
         block.classList.add('flex');
-        block.innerHTML = "" + this.area;
-        tab.insertAdjacentElement('afterbegin', block);
+        block.innerHTML = "" + this.name;
+        if (tab) {
+            tab.insertAdjacentElement('afterbegin', block);
+        }
     };
     Flat.prototype.printDescription = function () {
-        var block = document.createElement('p');
+        var description = document.createElement('div');
         var tab = document.querySelector('.table');
-        block.innerHTML = "" + this.description;
-        tab.insertAdjacentElement('afterbegin', block);
+        description.innerHTML = "\u041E\u043F\u0438\u0441: " + this.description;
+        if (tab) {
+            tab.insertAdjacentElement('afterbegin', description);
+        }
     };
     Flat.prototype.neighborhoodInfo = function () {
     };
     return Flat;
 }());
-var res = +prompt('Оберіть опцію: \n 1 - додати квартиру; \n 2- видалити квартиру; \n 3- редагування квартири; \n 4- вивести квартиру по району;\n 5- сортування квартир в порядку зростання вартості; \n 6- сортування квартир в порядку спадання вартості; \n *Увага! Введіть лише номер опції');
-var flat1 = new Flat(6, 1, 3, 81, 2019, "Center", 'Повністю закрита територія з власною охороною, цілодобове відеоспостереження, ландшафтний дизайн та місце для паркування авто.Ідеальний варіант для комфортного життя або здачі в оренду.ТОРГ реальним, зацікавленим покупцям', 45000);
-var flat2 = new Flat(6, 9, 1, 34, 2016, "Chayka", 'Простора, світла та стильна однокімнатна квартира в новобудові ЖК "Спектрум" (Реноме).Продумане планування з кухнею - вітальнею, велика засклена лоджія з гарним виглядом на приватну забудову та окрема спальня.Всі меблі та техніка залишаються.У квартирі найкраще технічне наповнення, автономне опалення з німецьким котлом Vaillant та вікна з німецького профілю Rehau.', 23000);
+var res = Number(prompt('Оберіть опцію: \n 1 - додати квартиру; \n 2- видалити квартиру; \n 3- редагування квартири; \n 4- вивести квартиру по району;\n 5- сортування квартир в порядку зростання вартості; \n 6- сортування квартир в порядку спадання вартості; \n *Увага! Введіть лише номер опції'));
+var flat1 = new Flat('Стильна 3- кімнатна квартира в новобудові з авторським ремонтом!', 6, 1, 3, 81, 2019, "Center", 'Повністю закрита територія з власною охороною, цілодобове відеоспостереження, ландшафтний дизайн та місце для паркування авто.Ідеальний варіант для комфортного життя або здачі в оренду.ТОРГ реальним, зацікавленим покупцям', 45000);
+var flat2 = new Flat('Продам 1-кім. квартиру .Новобудова.Євроремонт!', 6, 9, 1, 34, 2016, "Chayka", 'Простора, світла та стильна однокімнатна квартира в новобудові ЖК "Спектрум" (Реноме).Продумане планування з кухнею - вітальнею, велика засклена лоджія з гарним виглядом на приватну забудову та окрема спальня.Всі меблі та техніка залишаються.У квартирі найкраще технічне наповнення, автономне опалення з німецьким котлом Vaillant та вікна з німецького профілю Rehau.', 23000);
 var arrFlat = [flat1, flat2];
 switch (res) {
     case 1:
-        var newFlat = new Flat(+prompt('Кількість поверхів в будинку?\n *Увага! Дані мають містити лише цифри'), +prompt('На якому поверсі знаходиться квартира?\n *Увага! Дані мають містити лише цифри', ''), +prompt('Кількість кімнат у квартирі?\n *Увага! Дані мають містити лише цифри', ''), +prompt('Яка площа квартири?\n *Увага! Дані мають містити лише цифри', ''), +prompt('Рік будівництва будинку?\n *Увага! Дані мають містити лише цифри', ''), prompt('Район знаходження квартири', ''), prompt('Опишіть особливості та переваги квартири', ''), +prompt('Вартість квартири в доларах\n *Увага! Дані мають містити лише цифри', ''));
+        var newFlat = new Flat(String(prompt('Введіть назву квартири', '')), Number(prompt('Кількість поверхів в будинку?\n *Увага! Дані мають містити лише цифри')), Number(prompt('На якому поверсі знаходиться квартира?\n *Увага! Дані мають містити лише цифри', '')), Number(prompt('Кількість кімнат у квартирі?\n *Увага! Дані мають містити лише цифри', '')), Number(prompt('Яка площа квартири?\n *Увага! Дані мають містити лише цифри', '')), Number(prompt('Рік будівництва будинку?\n *Увага! Дані мають містити лише цифри', '')), String(prompt('Район знаходження квартири', '')), String(prompt('Опишіть особливості та переваги квартири', '')), Number(prompt('Вартість квартири в доларах\n *Увага! Дані мають містити лише цифри', '')));
         arrFlat.push(newFlat);
-        newFlat.printDescription();
+        arrFlat.forEach(function (flat) {
+            flat.printDescription();
+            flat.printAllInfo();
+        });
         break;
     case 2:
+        var delFlatIndex_1 = Number(prompt('Введіть номер з списку квартир, яку бажаєте видалити'));
+        arrFlat.forEach(function (flat, index) {
+            if (index == delFlatIndex_1 - 1) {
+                arrFlat.splice(delFlatIndex_1 - 1, 1);
+            }
+            flat.printDescription();
+        });
         break;
     case 3:
+        alert(arrFlat.forEach(function (flat) {
+            flat.printDescription();
+            flat.printAllInfo();
+        }));
         break;
     case 4:
         break;
@@ -48,10 +67,3 @@ switch (res) {
         break;
     default: alert('Бувай!');
 }
-// - Вивести інформацію про квартиру
-// - Опис квартири
-// = Додавання квартри
-// = Видалення квартри
-// = Редагування квартири
-// = Вивыд квартир по району
-// = Сортування по ціні (в обох напрмках)
--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ interface IFlat{
 }
     
 class Flat implements IFlat{
+    name: string;
     private floors: number;
     private rooms: number;
     private area: number;
@@ -12,7 +13,8 @@ class Flat implements IFlat{
     private price: number;
     private description: string;
     Floor: number;
-    constructor(floors: number, Floor: number,rooms: number, area: number, date: number, district: string, description: string, price: number){
+    constructor(name: string, floors: number, Floor: number,rooms: number, area: number, date: number, district: string, description: string, price: number){
+    this.name = name;
     this.floors = floors;
     this.Floor = Floor;
     this.rooms = rooms;
@@ -26,46 +28,68 @@ class Flat implements IFlat{
         let block = document.createElement('h1');
         let tab = document.querySelector('.table');
         block.classList.add('flex');
-        block.innerHTML = `${this.area}`; 
-        tab.insertAdjacentElement('afterbegin', block)
+        block.innerHTML = `${this.name}`; 
+        if (tab) {
+            tab.insertAdjacentElement('afterbegin', block);
+        }
     }
     printDescription(): void {
-        let block = document.createElement('p');
+        let description = document.createElement('div');
         let tab = document.querySelector('.table');
-        block.innerHTML = `${this.description}`; 
-        tab.insertAdjacentElement('afterbegin', block)
+        description.innerHTML = `Опис: ${this.description}`;
+        if (tab) {
+            tab.insertAdjacentElement('afterbegin', description)       
+        } 
     }
     neighborhoodInfo(): void {
 
     }
 }
 
-let res: number  = +prompt('Оберіть опцію: \n 1 - додати квартиру; \n 2- видалити квартиру; \n 3- редагування квартири; \n 4- вивести квартиру по району;\n 5- сортування квартир в порядку зростання вартості; \n 6- сортування квартир в порядку спадання вартості; \n *Увага! Введіть лише номер опції');
+let res: number  = Number(prompt ('Оберіть опцію: \n 1 - додати квартиру; \n 2- видалити квартиру; \n 3- редагування квартири; \n 4- вивести квартиру по району;\n 5- сортування квартир в порядку зростання вартості; \n 6- сортування квартир в порядку спадання вартості; \n *Увага! Введіть лише номер опції'));
 
-let flat1 = new Flat(6,1, 3, 81, 2019, "Center",'Повністю закрита територія з власною охороною, цілодобове відеоспостереження, ландшафтний дизайн та місце для паркування авто.Ідеальний варіант для комфортного життя або здачі в оренду.ТОРГ реальним, зацікавленим покупцям', 45000);
+let flat1 = new Flat('Стильна 3- кімнатна квартира в новобудові з авторським ремонтом!',6,1, 3, 81, 2019, "Center",'Повністю закрита територія з власною охороною, цілодобове відеоспостереження, ландшафтний дизайн та місце для паркування авто.Ідеальний варіант для комфортного життя або здачі в оренду.ТОРГ реальним, зацікавленим покупцям', 45000);
 
-let flat2 = new Flat(6,9, 1, 34, 2016, "Chayka",'Простора, світла та стильна однокімнатна квартира в новобудові ЖК "Спектрум" (Реноме).Продумане планування з кухнею - вітальнею, велика засклена лоджія з гарним виглядом на приватну забудову та окрема спальня.Всі меблі та техніка залишаються.У квартирі найкраще технічне наповнення, автономне опалення з німецьким котлом Vaillant та вікна з німецького профілю Rehau.',23000);
+let flat2 = new Flat('Продам 1-кім. квартиру .Новобудова.Євроремонт!',6,9, 1, 34, 2016, "Chayka",'Простора, світла та стильна однокімнатна квартира в новобудові ЖК "Спектрум" (Реноме).Продумане планування з кухнею - вітальнею, велика засклена лоджія з гарним виглядом на приватну забудову та окрема спальня.Всі меблі та техніка залишаються.У квартирі найкраще технічне наповнення, автономне опалення з німецьким котлом Vaillant та вікна з німецького профілю Rehau.',23000);
 
 let arrFlat: Flat[] = [flat1, flat2];
 
 switch(res){
     case 1:
         let newFlat = new Flat(
-            +prompt('Кількість поверхів в будинку?\n *Увага! Дані мають містити лише цифри', ), 
-            +prompt('На якому поверсі знаходиться квартира?\n *Увага! Дані мають містити лише цифри', ''), 
-            +prompt('Кількість кімнат у квартирі?\n *Увага! Дані мають містити лише цифри', ''), 
-            +prompt('Яка площа квартири?\n *Увага! Дані мають містити лише цифри', ''), 
-            +prompt('Рік будівництва будинку?\n *Увага! Дані мають містити лише цифри', ''), 
-            prompt('Район знаходження квартири', ''), 
-            prompt('Опишіть особливості та переваги квартири', ''),
-            +prompt('Вартість квартири в доларах\n *Увага! Дані мають містити лише цифри', '')
+            String(prompt('Введіть назву квартири', '')),
+            Number(prompt('Кількість поверхів в будинку?\n *Увага! Дані мають містити лише цифри', )), 
+            Number(prompt('На якому поверсі знаходиться квартира?\n *Увага! Дані мають містити лише цифри', '')), 
+            Number(prompt('Кількість кімнат у квартирі?\n *Увага! Дані мають містити лише цифри', '')), 
+            Number(prompt('Яка площа квартири?\n *Увага! Дані мають містити лише цифри', '')), 
+            Number(prompt('Рік будівництва будинку?\n *Увага! Дані мають містити лише цифри', '')), 
+            String(prompt('Район знаходження квартири', '')),
+            String(prompt('Опишіть особливості та переваги квартири', '')),
+            Number(prompt('Вартість квартири в доларах\n *Увага! Дані мають містити лише цифри', ''))
         );    
-        arrFlat.push(newFlat);   
-        newFlat.printDescription();
+        arrFlat.push(newFlat);  
+            arrFlat.forEach( flat => {
+                flat.printDescription();
+                flat.printAllInfo();
+            }
+        )
         break;
     case 2:
+      
+        let delFlatIndex = Number(prompt('Введіть номер з списку квартир, яку бажаєте видалити', ));
+            arrFlat.forEach((flat, index) => {
+                if (index == delFlatIndex-1) {
+                    arrFlat.splice(delFlatIndex-1, 1);
+                }
+                flat.printDescription()
+            })
         break;
     case 3:
+        alert(arrFlat.forEach( flat => {
+            flat.printDescription();
+            flat.printAllInfo();
+        }));
+
         break;
     case 4:
         break;
